832136718c0dea6747951b46c7e3618e097d864c
jugheadz <niloccruz@gmail.com>
2024-10-15 23:00:19 +0800

    Added anchor tag and log the clicked element

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -2,18 +2,26 @@ import ArticleTitle from "./articleTitle";
 import ArticleDetails from "./articleDetails";
 import Image from "./image";
 import ReadMoreButton from "./readMoreButton";
+import { useRef } from "react";
 
 const Article = ({
-  data: { title, content, image },
+  data: { title, content, image, url },
 }: {
-  data: { title: string; content: string; image: string };
+  data: { title: string; content: string; image: string; url: string; };
 }) => {
+    const articleRef = useRef(null);
+    const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault()
+        console.log(e.currentTarget, articleRef.current)
+    }
   return (
-    <article className="flex flex-col flex-1 gap-y-4 items-center justify-center md:items-start md:justify-start">
-      <Image src={image} />
-      <ArticleTitle title={title} />
-      <ArticleDetails content={content} />
-      <ReadMoreButton/>
+    <article className="flex-1 items-center justify-center md:items-start md:justify-start" ref={articleRef}>
+      <a onClick={handleClick} href={url} className="flex flex-col flex-1 gap-y-4 items-center justify-center md:items-start md:justify-start">
+        <Image src={image} />
+        <ArticleTitle title={title} />
+        <ArticleDetails content={content} />
+        <ReadMoreButton />
+      </a>
     </article>
   );
 };
